f797a4a86d6ae5d9fcc235a2516bd9e3f231be18
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2018-02-25 13:33:05 +0530

    Add files via upload

---
+++ C Programs/C Programs - 1/Infinite Coins.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main()
+{
+    int sum, no_of_A_coins;
+    scanf("%d %d", &sum, &no_of_A_coins);
+
+    printf(sum%500 <= no_of_A_coins ? "Yes\n" : "No\n");
+    return 0;
+}
