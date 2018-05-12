17c19e91e7e4235b144e102fd947ed9fef5625f5
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2018-05-12 19:29:21 +0530

    Add files via upload

---
+++ C Programs/C Programs - 2/Exponential.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    const int LIMIT = 1015;
+    vector <int> perfect_power(LIMIT, false);
+
+    perfect_power[1] = true;
+    for(int n = 2; n*n < LIMIT; n++)
+    {
+        int current_power = n*n;
+
+        while(current_power < LIMIT)
+        {
+            perfect_power[current_power] = true;
+            current_power *= n;
+        }
+    }
+
+    int X;
+    cin >> X;
+
+    while(!perfect_power[X])
+        X--;
+
+    cout << X;
+    return 0;
+}
