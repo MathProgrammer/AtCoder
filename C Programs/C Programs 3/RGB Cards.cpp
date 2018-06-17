381b3875402410ffe0752d5353ea6ecb9b5b1e2a
Saikat Ghosh <ghosh_saikat4000@yahoo.co.in>
2018-06-17 21:49:13 +0530

    Delete RGB Cards.cpp

--- C Programs/C Programs 3/RGB Cards.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-
-int main()
-{
-    int red, green, blue;
-    scanf("%d %d %d", &red, &green, &blue);
-    printf((100*red + 10*green + blue)%4 == 0 ? "YES\n" : "NO\n");
-    return 0;
-}
